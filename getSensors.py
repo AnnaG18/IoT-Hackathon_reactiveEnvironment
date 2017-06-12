84fe1ec5c627a5c1ce63a30477270e84083289b1
Bariumm <peter.kuehfuss@gmail.com>
2017-06-12 12:01:56 +0200

    Update getSensors.py
    -Home Assistant Python API
    -Add Config File
    -Refactoring
    -Ausgliederung von Funktionen
    -Mittelwert Berechnung
    -Dauerschleife
    -Temperatur abfrage
    -Co2 abfrage
    -Luftfeuchtigkeit abfrage

--- getSensors.py
+++ getSensors.py
@@ -5,19 +5,51 @@
 __author__      = "FarbenFroh"
 
 import homeassistant.remote as remote
+import configparser
+import time
 
 #Config
-HASS_IP = '192.168.1.188'
-HASS_PW = ''
-ROOM = "eg125"
+Config = configparser.ConfigParser()
+Config.read("config.conf")
+
+ROOM = Config.get('Sensors', 'Room')
 
 #Connect to Home Assistant
-api = remote.API(HASS_IP, HASS_PW)
+api = remote.API(Config.get('HomeAssistant', 'IP'), Config.get('HomeAssistant', 'PW'))
 
 #get all sensor entities
-print('\n-- Available entities:')
+
+temperature_ids = []
+co2_ids = []
+humidity_ids = []
+
 entities = remote.get_states(api)
 for entity in entities:
     if str(entity).__contains__(ROOM) and not str(entity).__contains__("group"):
-        print(entity)
+        #print(entity)
+        if str(entity).__contains__("TEMP"):
+            temperature_ids.append(entity.entity_id)
+        elif str(entity).__contains__("V_LEVEL"):
+            co2_ids.append(entity.entity_id)
+        elif str(entity).__contains__("HUM"):
+            humidity_ids.append(entity.entity_id)
 
+
+
+def getState(api, list):
+    i = 0
+    sum = 0
+    for list_item in list:
+        value = remote.get_state(api, list_item)
+        sum = sum + float(value.state)
+        i = i + 1
+    avg = sum/i
+    return avg
+
+run = True
+while run:
+    print(getState(api, temperature_ids))
+    print(getState(api, humidity_ids))
+    print(getState(api, co2_ids))
+
+    time.sleep(Config.get('System', 'Delay'))
