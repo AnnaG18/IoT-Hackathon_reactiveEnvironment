ec5cce565386ff323b07dc60c70538c043ff93b9
Bariumm <peter.kuehfuss@gmail.com>
2017-06-12 14:31:26 +0200

    Update farbenfroh.py
    -Value calculations
    -Light function
    -Value Dictionary

--- farbenfroh.py
+++ farbenfroh.py
@@ -15,7 +15,7 @@ Config.read("config.conf")
 #Connect to Home Assistant
 api = remote.API(Config.get('HomeAssistant', 'IP'), Config.get('HomeAssistant', 'PW'))
 
-#Create Lists for Sensor IDs
+#Create lists for sensor IDs
 temperature_ids = []
 co2_ids = []
 humidity_ids = []
@@ -48,23 +48,42 @@ def getStateAvg(api, list):
     return avg
 
 def setLightColor(api, values):
+
     print("Do Light")
-    print(values['temp_min'])
+    print("Temp Min: " + str(values['temp_min']))
+    print(values['temp_bright'])
+
+    print("Hum Max: " + str(values['hum_max']))
+    print(values['hum_bright'])
 
 #Permament loop
 run = True
 while run:
     #Get average values
-    values = {'temp_min':False, 'temp_min_bright':0,'temp_max':False,'temp_max_bright':0,'hum_min':False,'hum_min_bright':0,'hum_max':False,'hum_max_bright':0,'Co2_max':False,'Co2_max_bright':0}
+    values = {'temp_min':False,'temp_max':False,'temp_bright':0,'hum_min':False,'hum_max':False,'hum_bright':0,'Co2_max':False,'Co2_bright':0}
 
 
     tempAvg = getStateAvg(api, temperature_ids)
     if tempAvg < int(Config.get('Sectors', 'Temp_Min')):
         values['temp_min'] = True
+        values['temp_bright'] = (tempAvg - int(Config.get('Sectors', 'Temp_Min'))) * -10
     elif tempAvg > int(Config.get('Sectors', 'Temp_Max')):
         values['temp_max'] = True
+        values['temp_bright'] = (tempAvg - int(Config.get('Sectors', 'Temp_Max'))) * 10
 
-    getStateAvg(api, humidity_ids), getStateAvg(api, co2_ids)
+    humAvg = getStateAvg(api, humidity_ids)
+    if humAvg < int(Config.get('Sectors', 'Hum_Min')):
+        values['hum_min'] = True
+        values['hum_bright'] = (humAvg - int(Config.get('Sectors', 'Hum_Min'))) * -5
+    elif humAvg > int(Config.get('Sectors', 'Hum_Max')):
+        values['hum_max'] = True
+        values['hum_bright'] = (humAvg - int(Config.get('Sectors', 'Hum_Max'))) * 5
+
+    Co2Avg =  getStateAvg(api, co2_ids)
+    if Co2Avg > int(Config.get('Sectors', 'Co2_Max')):
+        values['Co2_max'] = True
+        values['Co2_bright'] = (Co2Avg - int(Config.get('Sectors', 'Co2_Max'))) * 500
+
     setLightColor(api, values)
 
     #Delay between call
